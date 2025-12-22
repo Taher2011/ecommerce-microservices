4f27f7646e2fef4f3299221bf7cbbae3c1391680
Taher <taherc.ali@gmail.com>
2025-12-22 21:03:53 +0530

    update S3

--- order-microservice/src/main/java/com/order/configuration/S3Config.java
+++ order-microservice/src/main/java/com/order/configuration/S3Config.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -13,26 +11,20 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
-	@Value("${aws.access-key:}")
-	private String accessKey;
-
-	@Value("${aws.secret-key:}")
-	private String secretKey;
-
 	@Value("${aws.s3.region:ap-south-1}")
 	private String region;
+	
+	@Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .build();
+    }
 
 	@Bean
-	public S3Client s3Client() {
-		AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
-		return S3Client.builder().region(Region.of(region))
-				.credentialsProvider(StaticCredentialsProvider.create(awsCreds)).build();
-	}
-
-	@Bean
-	public S3Presigner s3Presigner() {
-		AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
-		return S3Presigner.builder().region(Region.of(region))
-				.credentialsProvider(StaticCredentialsProvider.create(awsCreds)).build();
-	}
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .build();
+    }
 }
