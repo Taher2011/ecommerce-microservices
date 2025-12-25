20153602fa6afcf7d6101042e060c9424a9cb017
Taher <taherc.ali@gmail.com>
2025-12-25 12:03:28 +0530

    add cloudwatch bean and remove cloud from yml

--- order-microservice/src/main/java/com/order/configuration/CloudWatchMetricsConfig.java
+++ order-microservice/src/main/java/com/order/configuration/CloudWatchMetricsConfig.java
@@ -1,8 +1,13 @@
 package com.order.configuration;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.micrometer.cloudwatch2.CloudWatchConfig;
+import io.micrometer.cloudwatch2.CloudWatchMeterRegistry;
+import io.micrometer.core.instrument.Clock;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
 
@@ -12,5 +17,30 @@ public class CloudWatchMetricsConfig {
 	@Bean
 	public CloudWatchAsyncClient cloudWatchAsyncClient() {
 		return CloudWatchAsyncClient.builder().region(Region.US_EAST_1).build();
+	}
+
+	@Bean
+	public CloudWatchConfig cloudWatchConfig() {
+		return new CloudWatchConfig() {
+			@Override
+			public String get(String key) {
+				return null;
+			}
+
+			@Override
+			public String namespace() {
+				return "OrderServiceApp";
+			}
+
+			@Override
+			public Duration step() {
+				return Duration.ofMinutes(1);
+			}
+		};
+	}
+
+	@Bean
+	public CloudWatchMeterRegistry cloudWatchMeterRegistry(CloudWatchConfig config, CloudWatchAsyncClient client) {
+		return new CloudWatchMeterRegistry(config, Clock.SYSTEM, client);
 	}
 }
