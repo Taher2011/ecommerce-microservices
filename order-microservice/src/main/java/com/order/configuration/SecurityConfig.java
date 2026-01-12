8bcef9cd6d246ce77e3aa0cb7ee2b0a373194f25
Taher <taherc.ali@gmail.com>
2026-01-12 12:20:46 +0530

    update uri for atuator

--- order-microservice/src/main/java/com/order/configuration/SecurityConfig.java
+++ order-microservice/src/main/java/com/order/configuration/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/login", "/api/auth/refresh").permitAll()
-						.requestMatchers("/actuator/**").permitAll()
+						.requestMatchers("/api/actuator/**").permitAll()
 						/*
 						 * .requestMatchers(HttpMethod.GET,
 						 * "/api/orders/**").permitAll().requestMatchers("/redis-test/**") .permitAll()
